7e7e53b2f4b39bfa9ff23bb727dd34e9b040475a
Горлов Максим <gorlov.maxim@gmail.com>
2024-04-10 13:05:00 +0300

    Indents

--- termmodrinth/utils.py
+++ termmodrinth/utils.py
@@ -2,20 +2,20 @@ import datetime
 import hashlib
 
 def sizeof_fmt(num, suffix="B"):
-    for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
-        if abs(num) < 1024.0:
-            return f"{num:3.1f}{unit}{suffix}"
-        num /= 1024.0
-    return f"{num:.1f}Yi{suffix}"
+  for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
+    if abs(num) < 1024.0:
+      return f"{num:3.1f}{unit}{suffix}"
+    num /= 1024.0
+  return f"{num:.1f}Yi{suffix}"
 
 def convert_isoformat_date(strdate):
-    return datetime.datetime.strptime(strdate, "%Y-%m-%dT%H:%M:%S.%fZ")
+  return datetime.datetime.strptime(strdate, "%Y-%m-%dT%H:%M:%S.%fZ")
 
 def get_file_sha512(filename):
   h  = hashlib.sha512()
   b  = bytearray(128*1024)
   mv = memoryview(b)
   with open(filename, 'rb', buffering=0) as f:
-      while n := f.readinto(mv):
-          h.update(mv[:n])
+    while n := f.readinto(mv):
+      h.update(mv[:n])
   return h.hexdigest()
